625b4e85ba1381c80dea0b4cd73b8f120c591725
Andrew <andrew.fleer@gmail.com>
2021-01-03 12:52:07 -0600

    Added docs
    Docs are a good way to define a module and a function

--- circle.ex
+++ circle.ex
@@ -1,10 +1,13 @@
 defmodule Circle do
+  @moduledoc "Implements basic circle functions"
   @pi 3.14159
 
+  @doc "Defines the area of a circle."
   def area(r) do
     r * r * @pi
   end
 
+  @doc "Defines the circumference of a circle."
   def circumference(r) do
     2 * r * @pi
   end
