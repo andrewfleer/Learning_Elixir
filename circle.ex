03a87af60297f5087b9b48d47099499617c20533
Andrew <andrew.fleer@gmail.com>
2021-01-03 12:55:39 -0600

    Added comments
    Definitely one of the most important parts of any programming language!

--- circle.ex
+++ circle.ex
@@ -1,11 +1,13 @@
 defmodule Circle do
+  # This is how you do comments!
+
   @moduledoc "Implements basic circle functions"
   @pi 3.14159
 
   @doc "Defines the area of a circle."
   @spec area(number) :: number
   def area(r) do
-    r * r * @pi
+    r * r * @pi # You can also do inline comments
   end
 
   @doc "Defines the circumference of a circle."
