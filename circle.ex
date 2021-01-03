2905430ef17d3ccee2a722f82317f59b5ebcb644
Andrew <andrew.fleer@gmail.com>
2021-01-03 12:54:43 -0600

    Added type specs
    Typespecs provide a way of compensating for the lack of a static type system. This can be
    useful in conjunction with the dialyzer tool to perform static analysis of your programs.
    Furthermore, typespecs allow you to better-document your functions. Remember that Elixir
    is a dynamic language, so function inputs and output can’t be easily deduced by looking at
    the function’s signature

--- circle.ex
+++ circle.ex
@@ -3,11 +3,13 @@ defmodule Circle do
   @pi 3.14159
 
   @doc "Defines the area of a circle."
+  @spec area(number) :: number
   def area(r) do
     r * r * @pi
   end
 
   @doc "Defines the circumference of a circle."
+  @spec circumference(number) :: number
   def circumference(r) do
     2 * r * @pi
   end
